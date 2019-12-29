dce855e92edd8ed3455109392a02b01ef175ed18
thibault <thibault.latrace@gmail.com>
2019-12-29 12:19:03 +0100

    Ajout du fichier Python test pour tester le commit

---
+++ Test.py
@@ -0,0 +1,21 @@
+#Test avec un tutoriel sur internet
+
+#! /usr/bin/python
+# -*- coding:utf-8 -*-
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Coucou !"
+
+@app.route('/contact')
+def contact():
+    mail = "jean@bon.fr"
+    tel = "01 23 45 67 89"
+    return "Mail: {} --- Tel: {}".format(mail, tel)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
